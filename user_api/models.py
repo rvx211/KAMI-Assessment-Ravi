ced33f9b4b9d4716545198164d6412e8ae627dfd
Ravi Cheung <rvx211@github.com>
2023-12-14 21:17:11 +0700

    Task 02 - Add gitignore

--- user_api/models.py
+++ user_api/models.py
@@ -1,3 +1,5 @@
+from datetime import timezone
+
 from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
@@ -5,9 +7,10 @@ from django.contrib.auth.base_user import AbstractBaseUser
 # Create your models here.
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(_("email address"), unique=True)
+    email = models.EmailField(unique=True)
     password = models.passwordfield()
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
+    
     
