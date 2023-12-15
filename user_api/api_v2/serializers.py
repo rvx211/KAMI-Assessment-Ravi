67d2ac4dfa0bd339a8e6e1b69e615742ebc376a3
ravi <ravi@eksad.com>
2023-12-15 17:45:16 +0700

    Task 04 - Initial Serializer

--- user_api/api_v2/serializers.py
+++ user_api/api_v2/serializers.py
@@ -1,7 +1,15 @@
+"""This is the serializers module for the user object"""
 from rest_framework import serializers
-from user_api.models import User  
+from user_api.models import User
 
-class UserSerializer(serializers.ModelSerializer):
+class UserRegistrationRequestSerializer(serializers.ModelSerializer):
+    """This is serializer for user registration request.
+
+    Args:
+        serializers (object): Model serializers from Django REST API Framework
+    """
     class Meta:
+        """This is the meta class for user registration request serializers
+        """
         model = User
         fields = ['username', 'email', 'password']
