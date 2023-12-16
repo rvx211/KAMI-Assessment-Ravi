faea588580bb569376c4a6e9b60a1daf318c3d3e
Ravi Cheung <rvx211@github.com>
2023-12-17 02:41:13 +0700

    Task 07 - Add admin site

--- aircraft_api/admin.py
+++ aircraft_api/admin.py
@@ -1,3 +1,6 @@
+"""This is admin site for aircraft"""
 from django.contrib import admin
+from .models import Aircraft
 
 # Register your models here.
+admin.site.register(Aircraft)
--- user_api/admin.py
+++ user_api/admin.py
@@ -1,3 +1,6 @@
+"""This is admin site for aircraft"""
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+admin.site.register(User)
